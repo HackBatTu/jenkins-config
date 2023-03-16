e3ec4a0f9c10726c60702ec86fd551ac02913589
levantai1496 <levantai1496@gmail.com>
2023-03-16 11:59:51 +0700

    add newsource

--- src/main/java/com/fate/userservice/controller/UserController.java
+++ src/main/java/com/fate/userservice/controller/UserController.java
@@ -20,18 +20,16 @@ public class UserController {
     UserService userService;
 
     @PostMapping("/create")
-    public ResponseEntity<User> createUser(@RequestBody User user){
+    public ResponseEntity<User> createUser(@RequestBody User user) {
         User user1 = userService.saveUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(user1);
     }
-
     @GetMapping("/findById/{userId}")
     @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
     @Retry(name = "ratingHotelService", fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> findOne(@PathVariable Long userId){
         User user = userService.getUser(userId);
         return ResponseEntity.ok(user);
-
     }
     public ResponseEntity<User> ratingHotelFallback(Long userId, Exception ex) {
 //        logger.info("Fallback is executed because service is down : ", ex.getMessage());
@@ -39,7 +37,6 @@ public class UserController {
         User user = User.builder().email("dummy@gmail.com").name("Dummy").about("This user is created dummy because some service is down").id(1l).build();
         return new ResponseEntity<>(user, HttpStatus.BAD_REQUEST);
     }
-
     @GetMapping("/findAll")
     public ResponseEntity<List<User>> findAll(){
         List<User> userList = userService.getAllUser();
